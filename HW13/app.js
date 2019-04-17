5bee78abd86173995d90f115fc44fa920fe5a533
yarovojigor1988 <yarovojigor1988@gmail.com>
2019-04-17 22:08:48 +0300

    hw13

---
+++ HW13/app.js
@@ -0,0 +1,69 @@
+const form = document.forms['addTodoForm'];
+
+
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!title.value) return alertAddTodoToView();
+    newsDel();
+
+    console.log(title.value);
+    getNews(`https://newsapi.org/v2/top-headlines?q=${title.value}`, function (response) {
+        console.log(response);
+       
+        newsAddToWiev( newsAdd(response.articles));
+    });
+    form.reset();
+ 
+
+});
+
+function alertAddTodoToView() {
+    funcDel ();
+    const alertNew = alertAdd();
+    let alertMassege = document.querySelector('.col-6');
+    alertMassege.insertAdjacentHTML('afterbegin', alertNew);
+    setTimeout(funcDel, 2000);
+
+}
+
+
+function funcDel () {
+    if (document.querySelector(".alert") != null) {
+        const alertMassege = document.querySelector(".alert");
+        alertMassege.parentElement.removeChild(alertMassege);
+    }
+}
+
+function alertAdd() {
+    return `
+        <div class="alert alert-danger">
+        Введите Запрос!
+        </div>
+        `;
+    }
+
+
+
+
+function newsAdd(articles) {
+    let addLi= "";
+    for (i=0; i<articles.length; i++) {
+        addLi += `<li>${articles[i].title}</li>`;
+        console.log(12);
+    }
+    let addUl = `<ul class="newsList">` + addLi + `</ul>`;
+    return addUl;
+}
+
+function newsAddToWiev (callback){
+    form.insertAdjacentHTML('afterend', callback);
+}
+
+function newsDel () {
+    if (document.querySelector(".newsList") != null) {
+        const newsList = document.querySelector(".newsList");
+        newsList.parentElement.removeChild(newsList);
+    }
+}
---
+++ HW13/backapp.js
@@ -0,0 +1,9 @@
+function getNews (url, callback) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader('X-Api-Key', 'f0f4ea7d99c942fd82c8948ae799292a');
+    xhr.send();
+    xhr.addEventListener('load', () => callback(JSON.parse(xhr.responseText)));
+}
+
